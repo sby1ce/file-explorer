26e8f89c4e1b2040885ca541b7b31509eb0d4fb3
SomeBodY1CE <someonecallsyou@gmail.com>
2025-08-22 15:47:46 +0000

    refactor(ui): fuckass context menu abstractions

--- src/app/context_menu.rs
+++ src/app/context_menu.rs
@@ -2,12 +2,45 @@ use sycamore::prelude::*;
 use wasm_bindgen::{JsCast, prelude::Closure};
 use web_sys::{HtmlElement, MouseEvent, SubmitEvent};
 
-const ITEM_VIEW_SELECT: &str = "item-view-select";
-
 #[derive(Debug, Clone, Copy)]
 pub enum ItemView {
     Details,
     Grid,
+}
+
+#[component(inline_props)]
+fn ContextMenuOption(label: &'static str, click: impl Fn() + 'static) -> View {
+    view!(button(r#type="submit", on:click=move |_e| click()) {
+        (label)
+    })
+}
+
+#[component(inline_props)]
+fn ContextMenuItem(node_ref: NodeRef, children: Children, id: &'static str) -> View {
+    let styles = css_mod::get!("context_menu.css");
+    // have for styles unforuntately because can't pass generic static strings
+    let fieldset_style = format!(
+        "position-anchor: --{id}; inset-block-start: anchor(--{id} top); inset-inline-start: anchor(--{id} right)"
+    );
+    view! {
+        button(
+            r#type="button",
+            "popovertarget"=id,
+            "popovertargetaction"="show",
+            style=format!("anchor-name: --{id}"),
+        ) {
+            "item view"
+        }
+        fieldset(
+            id=id,
+            popover="",
+            r#ref=node_ref,
+            class=styles["fieldset"],
+            style=fieldset_style,
+        ) {
+            (children)
+        }
+    }
 }
 
 #[component(inline_props)]
@@ -19,16 +52,19 @@ pub fn ContextMenu() -> View {
 
     let abs_x: Signal<i32> = create_signal(0);
     let abs_y: Signal<i32> = create_signal(0);
-    let position = move || format!(
-        "top: {}px; left: {}px", abs_y.get(), abs_x.get(),
-    );
+    let position = move || format!("top: {}px; left: {}px", abs_y.get(), abs_x.get(),);
 
     let window = window();
     let on_context = Closure::wrap(Box::new(move |event: MouseEvent| {
         event.prevent_default();
         abs_x.set(event.page_x());
         abs_y.set(event.page_y());
-        popover.get().dyn_into::<HtmlElement>().unwrap().show_popover().unwrap();
+        popover
+            .get()
+            .dyn_into::<HtmlElement>()
+            .unwrap()
+            .show_popover()
+            .unwrap();
     }) as Box<dyn FnMut(MouseEvent)>);
     window
         .add_event_listener_with_callback("contextmenu", on_context.as_ref().unchecked_ref())
@@ -38,39 +74,33 @@ pub fn ContextMenu() -> View {
 
     let submit = move |event: SubmitEvent| {
         event.prevent_default();
-        popover.get().dyn_into::<HtmlElement>().unwrap().hide_popover().unwrap();
+        popover
+            .get()
+            .dyn_into::<HtmlElement>()
+            .unwrap()
+            .hide_popover()
+            .unwrap();
     };
+
+    let menu_items: [NodeRef; 1] = [create_node_ref()];
 
     view! {
         form(
             r#ref=popover,
-            class=styles["dialog"], 
-            style=position, 
+            class=styles["dialog"],
+            style=position,
             on:submit=submit,
             popover="",
         ) {
-            button(
-                r#type="button",
-                "popovertarget"=ITEM_VIEW_SELECT,
-                "popovertargetaction"="show",
-                style="anchor-name: --item-view-select",
-            ) { 
-                "item view" 
-            }
-            fieldset(
-                id=ITEM_VIEW_SELECT,
-                popover="",
-                class=styles["fieldset"],
-                style="position-anchor: --item-view-select; inset-block-start: anchor(--item-view-select top); inset-inline-start: anchor(--item-view-select right)"
-            ) {
-                button(
-                    r#type="submit",
-                    on:click=move |_e| item_view.set(ItemView::Details),
-                ) { "details" }
-                button(
-                    r#type="submit",
-                    on:click=move |_e| item_view.set(ItemView::Grid),
-                ) { "grid" }
+            ContextMenuItem(node_ref=menu_items[0], id="item-view-select") {
+                ContextMenuOption(
+                    label="details",
+                    click=move || item_view.set(ItemView::Details),
+                )
+                ContextMenuOption(
+                    label="grid",
+                    click=move || item_view.set(ItemView::Grid),
+                )
             }
         }
     }
